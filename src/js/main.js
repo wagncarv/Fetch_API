29effa5d88ddc8ab9cfc5d2f221a92ca5d4f6d9b
Wagner Carvalho <wagncarv@gmail.com>
2021-03-15 14:20:14 -0300

    all

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+const cep = document.querySelector("#cep")
+const error = document.querySelector("#error")
+const errMessage = (msg) =>{
+    error.classList.add("alert alert-danger")
+    error.append(msg)  
+}
+
+const showData = (result) => {
+    for(const field in result){
+        if(document.querySelector(`#${field}`)){
+            document.querySelector(`#${field}`).value = result[field]
+        }
+    }
+}
+
+cep.addEventListener("blur", (e)=>{
+    let search = cep.value.replace("-", "")
+
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/ `, options)
+    .then(response => {
+        response.json()
+        .then(data => {showData(data)})
+    })
+    .catch(err => errMessage(err.message))
+})
+
